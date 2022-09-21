24a3792bf4c99103327b922d7138c7e719159ae5
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-09-22 01:43:54 +0530

    feat: News, Blog, Status (#1)
    * fix stuff
    
    * check for method
    
    * update readme
    
    * Add LICENSE
    
    * Refactor & Add deno.news updates
    
    * Upload IV Rules
    
    * Update rules
    
    * fix: change timecheck to millisecond
    
    * fix: catch errors
    
    * refactor: re-organize routes
    
    * chore: clean up
    
    * chore: update readme
    
    * finalize
    
    * release test
    
    * fix: release api url
    
    * this is better
    
    hopefully

--- main.ts
+++ main.ts
@@ -1,69 +1,25 @@
-import { Bot } from "https://deno.land/x/grammy@v1.10.1/mod.ts";
-// import * as semver from "https://deno.land/std@0.152.0/semver/mod.ts";
+import { serve } from "https://deno.land/std@0.156.0/http/mod.ts";
+import { blog, news, release, status } from "./routes.ts";
 
-let lastSent = 0;
-const REPO = "denoland/deno";
-const RECIPIENT = Number(Deno.env.get("RECIPIENT"));
-const TOKEN = Deno.env.get("BOT_TOKEN");
-if (!TOKEN || isNaN(RECIPIENT)) {
-  throw new Error(
-    "Make sure you have provided BOT_TOKEN and RECIPIENT env vars",
-  );
-}
-const bot = new Bot(TOKEN);
+const SECRET = Deno.env.get("SECRET");
 
-interface Release {
-  id: number;
-  name: string;
-  tag_name: string;
-  html_url: string;
-  body?: string;
-}
-
-// Deno Deploy hack
-let skip = false;
-setInterval(async () => {
-  if (skip) {
-    skip = false;
-    return;
+serve(async (req) => {
+  const date = new Date(); // time the request recieved.
+  const paths = new URL(req.url).pathname.split("/");
+  const secretHeader = req.headers.get("secret");
+  if (SECRET !== undefined && secretHeader !== SECRET) {
+    return new Response("unauthorized");
   }
-
   try {
-    const release = await getLatestRelease();
-    if (release && lastSent !== release.id) {
-      lastSent = release.id;
-      console.log(`New release: ${release.tag_name}`);
-      // if (semver.patch(release.tag_name) == 0)
-      await bot.api.sendMessage(
-        RECIPIENT,
-        `<b>Deno ${esc(release.name)}</b>\
-${release.body ? `\n\n${esc(body(release.body))}...\n` : ""}
-<a href="${release.html_url}">View full release details</a>`,
-        { disable_web_page_preview: true, parse_mode: "HTML" },
-      );
+    if (req.method === "POST" && paths[1] === "status") {
+      await status(req);
+    } else if (paths[1] === "blog-news") {
+      await Promise.all([blog(date), news(date)]);
+    } else if (paths[1] === "release") {
+      await release(date);
     }
-  } catch (e) {
-    console.trace(e);
+  } catch (_err) {
+    //
   }
-
-  skip = true;
-}, 30.5 * 1000);
-
-async function getLatestRelease() {
-  const res = await fetch(
-    `https://api.github.com/repos/${REPO}/releases/latest`,
-  );
-  if (!res.ok) return;
-  return (await res.json()) as Release;
-}
-
-function esc(str: string) {
-  return str
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;");
-}
-
-function body(str: string) {
-  return str.split("\n").slice(2).join("\n").substring(0, 100);
-}
+  return new Response();
+});
