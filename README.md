0bcda6a8bce21940fc693b8397575659b29e7e25
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2023-03-11 23:56:28 +0530

    feat: gist kv

--- README.md
+++ README.md
@@ -1,22 +1,45 @@
 <samp>
 <!-- Yes, I like making it look monospaced. -->
 
-### Deno News
+**Deno News**
 
 Script for posting Deno (https://deno.land) related updates such as release
 info, blog posts, and other news from various official sources in a Telegram
-channel. Currently posting at https://t.me/deno_news, join for updates.
+channel. Currently posting at <https://t.me/deno_news>, join for updates.
 
-Sources (5):
+**Sources (5)**
 
-- Releases (https://github.com/denoland/deno/releases/latest)
-- Deno Blog (https://deno.com/blog)
-- Deno News (https://deno.news/archive)
-- Deno Status (https://denostatus.com)
-- TypeScript Blog (We &lt;3 TS) (https://devblogs.microsoft.com/typescript)
+- Releases (<https://github.com/denoland/deno/releases/latest>)
+- Deno Blog (<https://deno.com/blog>)
+- Deno News (<https://deno.news/archive>)
+- Deno Status (<https://denostatus.com>)
+- TypeScript Blog (We &lt;3 TS) (<https://devblogs.microsoft.com/typescript>)
+
+**Setup**
+
+You don't need to, if you're only looking for the realtime-updates. Just join:
+<https://t.me/deno_news>.
 
 Setup a cron-job for calling the main end point every 1 minute. You need to set
-BOT_TOKEN, CHANNEL environmental variables. If you want to, you can also set a
-SECRET, which should be also added in the headers when requesting the server.
+the following environmental variables to make this work properly:
+
+- BOT_TOKEN: Bot token of the Telegram bot.
+- CHANNEL: Chat ID where the bot is allowed to send and pin messages (only
+  release news are pinned).
+
+and this project uses a stupid KV store powered by GitHub Gists
+([Source Code](https://gist.github.com/dcdunkan/36b6329408f3a2a91881fa29c8e08c30)).
+Why not an actual, proper KV store or DB? Idk, this is simply a tool and its not
+aimed for general use. So I took the opportunity to mess around with Gists.
+Anyway,
+
+- GITHUB_PAT: GitHub's personal access token with `gists` scope enabled.
+- GIST_RAW_URL: Yes, a raw URL to the Gist file. The required args will be
+  parsed from it. It should be in the format:
+  <https://gist.githubusercontent.com/owner/gist_id/raw(/revision)/file.json>
+  (You can simply copy the URL of the `RAW` button shown with the file)
+
+If you want to, you can also set a SECRET, which should be also added in the
+headers when requesting the server.
 
 </samp>
--- main.ts
+++ main.ts
@@ -1,32 +1,41 @@
 /**
- * Script for fetching Deno (https://deno.land/) related RSS feeds and
+ * Script for fetching Deno (https://deno.land) related RSS feeds and
  * posting them in a Telegram channel. Requires a cron-job calling the
  * main end point every 1 minute. The feed to be fetched is automatically
- * selected one-by-one. THIS DOES NOT USE A DATABASE. So make sure the
- * cron job is working properly. Join https://t.me/deno_news for demo.
+ * selected one-by-one. Join https://t.me/deno_news for demo.
  * IV Rules are in the rules/ directory. 'secret' header should be set
  * when calling the end point (if SECRET is set in environmental variables).
  *
- * Licensed under MIT | Copyright (c) 2022-2023 Dunkan
+ * Licensed under MIT | Copyright (c) 2022-Present Dunkan
  * GitHub Repository: https://github.com/dcdunkan/deno-bot
  */
 
-import { load } from "https://deno.land/std@0.177.0/dotenv/mod.ts";
-import { serve } from "https://deno.land/std@0.177.0/http/mod.ts";
+import { load } from "https://deno.land/std@0.179.0/dotenv/mod.ts";
+import { serve } from "https://deno.land/std@0.179.0/http/mod.ts";
 import { parseFeed } from "https://deno.land/x/rss@0.5.8/mod.ts";
-import { Bot } from "https://deno.land/x/grammy@v1.14.1/mod.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.15.1/mod.ts";
+import { GistKV } from "https://gist.githubusercontent.com/dcdunkan/36b6329408f3a2a91881fa29c8e08c30/raw/c89f5567a3279ff4693aafa3b6c8f7260351cc02/gist.ts";
 
 await load({ export: true });
 const env = Deno.env.toObject() as {
   BOT_TOKEN: string;
-  SECRET: string;
+  GITHUB_PAT: string;
+  GIST_RAW_URL: string;
+  SECRET?: string;
 };
+
+const parts = new URL(env.GIST_RAW_URL).pathname.split("/");
+const kv = new GistKV({
+  token: env.GITHUB_PAT,
+  gistId: parts[2],
+  fileName: parts[parts.length === 6 ? 5 : 4],
+});
 
 const bot = new Bot(env.BOT_TOKEN);
 const CHANNEL = Number(Deno.env.get("CHANNEL"));
-if (isNaN(CHANNEL)) throw new Error("CHANNEL should be a channel ID");
+if (isNaN(CHANNEL)) throw new Error("CHANNEL should be a chat (channel) ID");
 const ZWSP = "\u200b"; // zero-width space character for IV.
-// See rules/ directory for the sources.
+// See the rules/ directory for the sources.
 const RHASHES: Record<string, string> = {
   "deno.news": "b5ba1c523db473", // https://deno.news/archive/...
   "deno.com": "28aee3eda1037a", // https://deno.com/blog/...
@@ -42,7 +51,7 @@ const URLS = {
 
 const handlers: Record<string, () => Promise<string[]>> = {
   "blog": async () => {
-    const entries = await getLatestEntries(URLS.blog);
+    const entries = await getLatestEntries("blog");
     return entries.map((entry) => {
       const title = entry.title?.value!;
       const url = entry.links[0].href ?? entry.id;
@@ -50,7 +59,7 @@ const handlers: Record<string, () => Promise<string[]>> = {
     });
   },
   "news": async () => {
-    const entries = await getLatestEntries(URLS.news);
+    const entries = await getLatestEntries("news");
     return entries.map((entry) => {
       const title = entry.title?.value!;
       const url = (entry.links[0].href ?? entry.id)
@@ -59,7 +68,7 @@ const handlers: Record<string, () => Promise<string[]>> = {
     });
   },
   "status": async () => {
-    const entries = await getLatestEntries(URLS.status);
+    const entries = await getLatestEntries("status");
     return entries.map((entry) => {
       const title = entry.title?.value!;
       const url = entry.links[0].href ?? entry.id;
@@ -67,15 +76,16 @@ const handlers: Record<string, () => Promise<string[]>> = {
     });
   },
   "release": async () => {
-    const lastChecked = getLastChecked();
+    const lastChecked = await kv.get<number>("denonews_release");
+    if (!lastChecked) return [];
     const response = await fetch(URLS.release);
     const release = await response.json();
-    const published = new Date(release.published_at);
-    if (published <= lastChecked) return [];
+    if (release.id === lastChecked) return [];
+    await kv.set("denonews_release", release.id);
     return [`<b>${esc(release.name)}</b>\n\n${esc(release.html_url)}`];
   },
   "typescript": async () => {
-    const entries = await getLatestEntries(URLS.typescript);
+    const entries = await getLatestEntries("typescript");
     return entries.map((entry) => {
       const title = entry.title?.value!;
       const url = entry.links[0].href ?? entry.id;
@@ -86,24 +96,20 @@ const handlers: Record<string, () => Promise<string[]>> = {
 
 const ROUTES = Object.keys(handlers);
 
-function getLastChecked() {
-  const now = new Date();
-  now.setSeconds(0);
-  now.setMilliseconds(0);
-  // This is why cron job with 1 minute interval is required.
-  return new Date(now.valueOf() - (ROUTES.length * 60 * 1000) - 2);
-}
-
-async function getLatestEntries(url: string) {
-  const lastChecked = getLastChecked();
-  const response = await fetch(url);
+async function getLatestEntries(page: keyof typeof URLS) {
+  const lastChecked = await kv.get<string>(`denonews_${page}`);
+  if (!lastChecked) return [];
+  const response = await fetch(URLS[page]);
   const textFeed = await response.text();
   const feed = await parseFeed(textFeed);
   const entries: typeof feed.entries = [];
   for (const entry of feed.entries) {
-    if (!entry.published) continue;
-    if (entry.published <= lastChecked) break;
+    if (!entry.id) continue;
+    if (entry.id === lastChecked) break;
     entries.unshift(entry); // FIFO
+  }
+  if (entries.length > 0) {
+    await kv.set(`denonews_${page}`, entries.at(-1)!.id);
   }
   return entries;
 }
